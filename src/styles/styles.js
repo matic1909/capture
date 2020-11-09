30964f1c61f7585719a58f005be2b2d973e536a2
Nils Matic <matic1909@gmail.com>
2020-11-09 19:30:34 +0100

    styled Services section

---
+++ src/styles/styles.js
@@ -0,0 +1,34 @@
+const { default: styled } = require("styled-components");
+
+const Section = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+export { Section, Hide, Description, Image };
