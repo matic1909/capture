ad4094eaebad3815e6e85d66f3f26ebbcc0d3aa4
Nils Matic <matic1909@gmail.com>
2020-11-09 19:36:40 +0100

    added FAQ section styling

--- src/components/FAQSection.jsx
+++ src/components/FAQSection.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import styled from "styled-components";
+import { Section, Description, Hide, Image } from "../styles/styles";
 
 const FAQSection = () => {
   return (
-    <div className="">
+    <FAQ>
       <h2>
         Any Questions? <span>FAQ</span>
       </h2>
@@ -14,6 +16,7 @@ const FAQSection = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, sequi.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Daily Schedule</h4>
@@ -23,6 +26,7 @@ const FAQSection = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, sequi.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Different Payment Methods</h4>
@@ -32,6 +36,7 @@ const FAQSection = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, sequi.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What Products Do You Offer?</h4>
@@ -41,9 +46,37 @@ const FAQSection = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, sequi.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </FAQ>
   );
 };
+
+const FAQ = styled(Section)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #ccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+  }
+  p {
+    padding: 1rem 0rem;
+  }
+`;
 
 export default FAQSection;
