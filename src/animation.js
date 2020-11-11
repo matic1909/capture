8a7aea9ea0fec49e8f1f39abc6f7486fe237ac0f
Nils Matic <matic1909@gmail.com>
2020-11-11 19:55:59 +0100

    added animations to OurWork page

--- src/animation.js
+++ src/animation.js
@@ -7,14 +7,13 @@ export const pageAnimation = {
     opacity: 1,
     y: 0,
     transition: {
-      duration: 1,
+      duration: 0.5,
       when: "beforeChildren",
       staggerChildren: 0.25,
     },
   },
   exit: {
     opacity: 0,
-    y: 300,
     duration: 0.5,
   },
 };
@@ -42,4 +41,23 @@ export const photoAnimation = {
     opacity: 1,
     transition: { ease: "easeOut", duration: 0.75 },
   },
+};
+
+export const lineAnimation = {
+  hidden: { width: 0 },
+  show: { width: "100%", transition: { duration: 0.5 } },
+};
+
+export const slider = {
+  hidden: { x: "-130%", skew: "45deg" },
+  show: {
+    x: "100%",
+    skew: "0deg",
+    transition: { ease: "easeOut", duration: 1 },
+  },
+};
+
+export const sliderContainer = {
+  hidden: { opacity: 1 },
+  show: { opacity: 1, transition: { staggerChildren: 0.15, ease: "easeOut" } },
 };
